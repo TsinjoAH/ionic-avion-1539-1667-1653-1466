6705f17327f5175441e865e74a01afa73c6ec192
TsinjoAH <52tsinjo@gmail.com>
2022-12-20 15:11:22 +0300

    Update global_config.ts

--- fleet/src/data/global_config.ts
+++ fleet/src/data/global_config.ts
@@ -1,6 +1,6 @@
 
 
-const apiUrl = "http://localhost:8080/";
+const apiUrl = "https://ws-avion-1539-1667-1653-1466-production.up.railway.app/";
 const baseUrl = (str: string):string => apiUrl + str;
 
 export {baseUrl}
